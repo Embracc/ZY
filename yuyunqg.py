49cbfe307493a17f108c6cbc7f09082c4926be53
Embracc <81205828+Embracc@users.noreply.github.com>
2023-04-15 15:35:39 +0800

    Update yuyunqg.py

--- yuyunqg.py
+++ yuyunqg.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-cron: 59 59 19 * * *
+cron: 58 59 19 * * *
 new Env('雨云抢机子');
 """
+
 import requests
 import os
 import http.client
-
+import time
 
 yykey = os.environ['yykey']
 conn = http.client.HTTPSConnection("api.v2.rainyun.cc:36688")
@@ -15,8 +16,14 @@ headers = {
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.39",
     'x-api-key': yykey,
     'content-type': "application/json"
-    }
-conn.request("POST", "/user/reward/items", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+}
+
+for i in range(5):
+    conn.request("POST", "/user/reward/items", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+
+    # 等待20毫秒
+    #time.sleep(0.02)
+
